bfbef34c51c3582d8c4197f64a7e738d48c9c43b
Brie Bencich <bbencich@hotmail.com>
2020-11-20 00:31:01 +0200

    Testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,5 @@
-const store = require("../Develop/db/store");
-const { read } = require("../Develop/db/store");
-
-const router = require('express').Router(); 
-const store = require('../Develop/db/store'); 
+const router = require('express').Router();
+const store = require('../Develop/db/store');
 
 
 router.get('/notes', (req, res) => { 
@@ -14,7 +11,7 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err)); 
 }); 
 
-router.post('/notes', (req, res) => { 
+router.post('/notes/:id', (req, res) => { 
     store 
     .addNote(req.body)
     .then((note) => res.json(note))
