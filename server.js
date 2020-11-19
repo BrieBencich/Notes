4fa8b0601b024bf3263643f5b13a804e7ccc661e
Brie Bencich <bbencich@hotmail.com>
2020-11-19 23:36:40 +0200

    adding server side

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const apiRoutes = require('./routes/apiRoutes'); 
+const htmlRoutes = require('./routes/htmlRoutes'); 
+
+//Create App/PORT 
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: treu})); 
+app.use(express.static('public')); 
+app.use('/api', apiRoutes); 
+app.use('/', htmlRoutes); 
+
+//Start PORT 
+app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`)); 
+
